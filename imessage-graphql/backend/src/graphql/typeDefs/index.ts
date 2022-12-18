ded85743d274bbee1f7d676c6d9752ed764bd69e
AbenezerKitata <92657524+AbenezerKitata@users.noreply.github.com>
2022-12-17 17:26:00 -0800

    Delete imessage-graphql directory

--- imessage-graphql/backend/src/graphql/typeDefs/index.ts
+++
@@ -1,7 +0,0 @@
-import userTypeDefs from "./users";
-import conversationTypeDefs from "./conversations";
-import messageTypeDefs from "./messages";
-
-const typeDefs = [userTypeDefs, conversationTypeDefs, messageTypeDefs];
-
-export default typeDefs;
