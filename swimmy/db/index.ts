5edd2f4d9dea3fab99ca8c6e2735180e7d541ed7
AbenezerKitata <92657524+AbenezerKitata@users.noreply.github.com>
2022-12-17 17:26:31 -0800

    Delete swimmy directory

--- swimmy/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-import { enhancePrisma } from "blitz"
-
-const EnhancedPrisma = enhancePrisma(PrismaClient)
-
-export * from "@prisma/client"
-
-export default new EnhancedPrisma()
--- swimmy/interface/resolvers/logout.ts
+++
@@ -1,7 +0,0 @@
-import { MutationResolvers } from "interface/__generated__/node"
-
-export const logout: MutationResolvers["logout"] = async (_, args, ctx) => {
-  await ctx.session?.$revoke()
-
-  return null
-}
